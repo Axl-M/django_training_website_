585235f7ccff984132c1866820161ac77aa36a38
AxL <axlmalyasov@gmail.com>
2021-10-23 13:38:41 +1000

    add FORM "Добавление новости"

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
 from .models import News, Category
+from .forms import NewsForm
 
 # def index(request):
 #     # print(dir(request))
@@ -28,7 +29,17 @@ def get_category(request, category_id):
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def test(request):
-    return HttpResponse('<h1> Тестовая страница </h1>')
+# def test(request):
+#     return HttpResponse('<h1> Тестовая страница </h1>')
 
-# Create your views here.
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item':news_item})
+
+def add_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
